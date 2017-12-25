702d6f4f83ebb6e0061062c1be072c181080e37f
dufufeisdu <fufei.du@gmail.com>
2017-12-24 19:25:15 -0800

    (feat)Set up database

--- chinesefood/snacks/models.py
+++ chinesefood/snacks/models.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.auth.validators import ASCIIUsernameValidator
 # Create your models here.
+# snacks: name origin flavor cookingstep whereTobuy
+
+
+class Snack(models.Model):
+    snack_name = models.CharField(max_length=200)
+    snack_origin = models.CharField(max_length=200)
+    snack_flavor = models.CharField(max_length=200)
+    snack_cooking_step = models.CharField(max_length=200)
+    snack_image_url = models.CharField(max_length=200)
+    snack_liked = models.IntegerField(default=0)
+
+
+class UserLikesSnack(models.Model):
+    snack_name =
+
+
+class CustomUser(User):
+    username_validator = ASCIIUsernameValidator()
+
+    class Meta:
+        proxy = True  # If no new field is added.
